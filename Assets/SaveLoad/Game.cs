0e7202368bf2cdcdf432bdee605a2c343989c6ea
Fanakartal - METU <fanakartal@gmail.com>
2015-02-09 16:08:56 +0200

    package eklendi.

---
+++ Assets/SaveLoad/Game.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class _Game 
+{ //don't need ": Monobehaviour" because we are not attaching it to a game object
+
+	public static _Game current;
+	public _Character knight;
+	public _Character rogue;
+	public _Character wizard;
+
+	public _Game () 
+    {
+		knight = new _Character();
+		rogue = new _Character();
+		wizard = new _Character();
+	}
+		
+}
---
+++ Assets/SaveLoad/SaveLoad.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class _SaveLoad 
+{
+
+	public static List<_Game> savedGames = new List<_Game>();
+			
+	//it's static so we can call it from anywhere
+	public static void Save() 
+    {
+		_SaveLoad.savedGames.Add(_Game.current);
+		
+        BinaryFormatter bf = new BinaryFormatter();
+		
+        //Application.persistentDataPath is a string, so if you wanted you can put that into debug.log if you want to know where save games are located
+		FileStream file = File.Create (Application.persistentDataPath + "/savedGames.gd");
+        Debug.Log(Application.persistentDataPath);
+        //you can call it anything you want
+		
+        bf.Serialize(file, _SaveLoad.savedGames);
+		
+        file.Close();
+	}	
+	
+	public static void Load() 
+    {
+		if(File.Exists(Application.persistentDataPath + "/savedGames.gd")) 
+        {
+			BinaryFormatter bf = new BinaryFormatter();
+			
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+			
+            _SaveLoad.savedGames = (List<_Game>)bf.Deserialize(file);
+			
+            file.Close();
+		}
+	}
+}
